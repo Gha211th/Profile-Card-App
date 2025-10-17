c75d4abb2847c2e55025c80a2cf6b4d180099ee5
ghatraxion <agh.arrafif@gmail.com>
2025-10-17 08:32:17 +0700

    dart

---
+++ lib/doc_about.dart
@@ -0,0 +1,28 @@
+/**
+ * PENJELASAN
+ * 
+ * struktur flutter terdiri dari:
+ * 1. .dart tool
+ * 2. .idea
+ * 3. android
+ *  => untuk kofigurasi android serta
+ *  => library android (native)
+ * 4. Build
+ *  => hasil compile
+ * 5. ios
+ *  => untuk konfigurasi ios serta
+ *  => library ios (native)
+ * 6. lib
+ *  => tempat file dart berada yang nantinya
+ *  => sering digunakan
+ * 7. linux
+ *  => untuk konfigurasi linux
+ * 8. macos
+ *  => untuk konfigurasi macos
+ * 9. test
+ *  => tempat file untuk testing aplikasi
+ * 10. web
+ *  => untuk konfigurasi web
+ * 11. windows
+ *  => untuk konfigurasi windows
+ */
