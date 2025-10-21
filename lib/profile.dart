c9169878784462e918b995757cc86d387d10b667
ghatraxion <agh.arrafif@gmail.com>
2025-10-21 13:08:09 +0700

    first app

--- lib/profile.dart
+++ lib/profile.dart
@@ -5,6 +5,6 @@ class ProfileCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(home: Scaffold(), debugShowCheckedModeBanner: false);
   }
 }
