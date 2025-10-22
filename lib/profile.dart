ac25f662fb334e2f9373212278dd877659c77557
ghatraxion <agh.arrafif@gmail.com>
2025-10-22 11:20:36 +0700

    flutter

--- lib/profile.dart
+++ lib/profile.dart
@@ -6,27 +6,7 @@ class ProfileCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text("nothing happen")),
-        body: Column(
-          children: [
-            Container(
-              width: 200,
-              child: Text(
-                "Shell is going sure?",
-                style: TextStyle(
-                  color: Colors.amber,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  backgroundColor: Colors.black,
-                  fontStyle: FontStyle.italic,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: Scaffold(appBar: AppBar(), body: Center()),
     );
   }
 }
