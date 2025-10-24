5916414589b13f8ae44c5ee1ed033c1ec13acc56
ghatraxion <agh.arrafif@gmail.com>
2025-10-24 13:59:00 +0700

    i hope that i can understand:)

--- lib/profile.dart
+++ lib/profile.dart
@@ -36,37 +36,53 @@ class ProfileCard extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 10),
-              Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, magna ut convallis gravida, turpis erat sollicitudin felis, non pulvinar justo purus et ipsum. Sed sodales, neque nec eleifend mattis, velit nisi ultrices felis, vitae egestas odio magna sed libero. Integer ac porttitor nisl. Vivamus ut cursus tortor. Mauris sit amet mattis elit. Duis commodo orci id tellus porttitor, vel rhoncus nulla fermentum. Donec sed lacus id sapien interdum ultricies. Curabitur pretium, risus at porttitor gravida, augue justo eleifend justo, eget viverra elit purus non lorem. Nunc suscipit felis vel ligula lacinia, nec tristique lorem tincidunt.",
-                style: TextStyle(color: Colors.black, fontSize: 10),
-                textAlign: TextAlign.justify,
+              Container(
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 222, 89, 1),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: 
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, magna ut convallis gravida, turpis erat sollicitudin felis, non pulvinar justo purus et ipsum. Sed sodales, neque nec eleifend mattis, velit nisi ultrices felis, vitae egestas odio magna sed libero. Integer ac porttitor nisl. Vivamus ut cursus tortor. Mauris sit amet mattis elit. Duis commodo orci id tellus porttitor, vel rhoncus nulla fermentum. Donec sed lacus id sapien interdum ultricies. Curabitur pretium, risus at porttitor gravida, augue justo eleifend justo, eget viverra elit purus non lorem. Nunc suscipit felis vel ligula lacinia, nec tristique lorem tincidunt.",
+                    style: TextStyle(color: Colors.black, fontSize: 10),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
               ),
               SizedBox(height: 15),
-              Row(
-                children: [
-                  Container(
-                    width: 60,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(255, 222, 89, 1),
-                      borderRadius: BorderRadius.circular(5),
+              Container(
+                decoration: BoxDecoration(),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 222, 89, 1),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Icon(Icons.android, color: Colors.white, size: 30),
                     ),
-                    child: Icon(Icons.android, color: Colors.white, size: 30),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Android Dev",
-                          style: TextStyle(fontWeight: FontWeight.w600),
-                        ),
-                        Text("100 APK Project", style: TextStyle(fontSize: 8)),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Android Dev",
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                          Text(
+                            "100 APK Project",
+                            style: TextStyle(fontSize: 8),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ],
           ),
