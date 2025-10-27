040a5af9f042fa394e415d1e28f1efb243c7fbab
ghatraxion <agh.arrafif@gmail.com>
2025-10-27 09:58:03 +0700

    woke!!!

--- lib/profile.dart
+++ lib/profile.dart
@@ -18,199 +18,232 @@ class ProfileCard extends StatelessWidget {
         ),
         backgroundColor: Colors.black,
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 40.0, right: 40.0, top: 20.0),
-        child: Center(
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 3),
-                  boxShadow: [
-                    BoxShadow(color: Colors.black, offset: Offset(6, 6)),
-                  ],
-                ),
-                child: Image.asset(
-                  'assets/profile.png',
-                  height: 150,
-                  width: 150,
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(255, 72, 72, 1),
-                  border: Border.all(color: Colors.black, width: 2),
-                  boxShadow: [
-                    BoxShadow(color: Colors.black, offset: Offset(4, 4)),
-                  ],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    top: 5.0,
-                    bottom: 5.0,
-                    left: 15.0,
-                    right: 15.0,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 40.0, right: 40.0, top: 20.0),
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 3),
+                    boxShadow: [
+                      BoxShadow(color: Colors.black, offset: Offset(6, 6)),
+                    ],
                   ),
-                  child: Text(
-                    "Agha Ar-Rafif".toUpperCase(),
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w800,
-                      fontFamily: 'RobotoMono',
+                  child: Image.asset(
+                    'assets/profile.png',
+                    height: 150,
+                    width: 150,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(255, 72, 72, 1),
+                    border: Border.all(color: Colors.black, width: 2),
+                    boxShadow: [
+                      BoxShadow(color: Colors.black, offset: Offset(4, 4)),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 5.0,
+                      bottom: 5.0,
+                      left: 15.0,
+                      right: 15.0,
+                    ),
+                    child: Text(
+                      "Agha Ar-Rafif".toUpperCase(),
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800,
+                        fontFamily: 'RobotoMono',
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 8),
-              Container(
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(102, 161, 216, 1),
-                  border: Border.all(color: Colors.black, width: 2),
-                  boxShadow: [
-                    BoxShadow(color: Colors.black, offset: Offset(4, 4)),
-                  ],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    top: 5.0,
-                    bottom: 5.0,
-                    left: 20.0,
-                    right: 20.0,
+                SizedBox(height: 8),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(102, 161, 216, 1),
+                    border: Border.all(color: Colors.black, width: 2),
+                    boxShadow: [
+                      BoxShadow(color: Colors.black, offset: Offset(4, 4)),
+                    ],
                   ),
-                  child: Text(
-                    "~ Student of SMP IDN BS SOLO ~",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 10,
-                      fontWeight: FontWeight.w600,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 5.0,
+                      bottom: 5.0,
+                      left: 20.0,
+                      right: 20.0,
+                    ),
+                    child: Text(
+                      "~ Student of SMP IDN BS SOLO ~",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 10,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(255, 222, 89, 1),
-                  border: Border.all(color: Colors.black, width: 2),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 0,
-                      color: Colors.black,
-                      offset: const Offset(6, 6),
+                SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(255, 222, 89, 1),
+                    border: Border.all(color: Colors.black, width: 2),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 0,
+                        color: Colors.black,
+                        offset: const Offset(6, 6),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, magna ut convallis gravida, turpis erat sollicitudin felis, non pulvinar justo purus et ipsum. Sed sodales, neque nec eleifend mattis, velit nisi ultrices felis, vitae egestas odio magna sed libero. Integer ac porttitor nisl. Vivamus ut cursus tortor. Mauris sit amet mattis elit. Duis commodo orci id tellus porttitor, vel rhoncus nulla fermentum. Donec sed lacus id sapien interdum ultricies. Curabitur pretium, risus at porttitor gravida, augue justo eleifend justo, eget viverra elit purus non lorem. Nunc suscipit felis vel ligula lacinia, nec tristique lorem tincidunt.",
+                      style: TextStyle(color: Colors.black, fontSize: 10),
+                      textAlign: TextAlign.justify,
                     ),
-                  ],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, magna ut convallis gravida, turpis erat sollicitudin felis, non pulvinar justo purus et ipsum. Sed sodales, neque nec eleifend mattis, velit nisi ultrices felis, vitae egestas odio magna sed libero. Integer ac porttitor nisl. Vivamus ut cursus tortor. Mauris sit amet mattis elit. Duis commodo orci id tellus porttitor, vel rhoncus nulla fermentum. Donec sed lacus id sapien interdum ultricies. Curabitur pretium, risus at porttitor gravida, augue justo eleifend justo, eget viverra elit purus non lorem. Nunc suscipit felis vel ligula lacinia, nec tristique lorem tincidunt.",
-                    style: TextStyle(color: Colors.black, fontSize: 10),
-                    textAlign: TextAlign.justify,
                   ),
                 ),
-              ),
-              SizedBox(height: 15),
-              Container(
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(104, 180, 143, 1),
-                  border: Border.all(color: Colors.black, width: 2),
-                  boxShadow: [
-                    BoxShadow(color: Colors.black, offset: Offset(6, 6)),
-                  ],
+                SizedBox(height: 15),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(104, 180, 143, 1),
+                    border: Border.all(color: Colors.black, width: 2),
+                    boxShadow: [
+                      BoxShadow(color: Colors.black, offset: Offset(6, 6)),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 60,
+                        height: 60,
+                        child: Icon(
+                          Icons.android,
+                          color: Colors.black,
+                          size: 40,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Android Dev",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black,
+                                fontFamily: 'RobotoMono',
+                              ),
+                            ),
+                            Text(
+                              "100 APK Project",
+                              style: TextStyle(
+                                fontSize: 8,
+                                fontFamily: 'RobotoMono',
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                child: Row(
+                Container(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 5,
+                    ),
+                    child: Text(
+                      "~ Schedule ~",
+                      style: TextStyle(
+                        fontFamily: 'RobotoMono',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Container(
-                      width: 60,
-                      height: 60,
-                      child: Icon(Icons.android, color: Colors.black, size: 40),
+                    Column(
+                      children: [
+                        Text(
+                          'Ngoding',
+                          style: TextStyle(
+                            fontFamily: 'RobotoMono',
+                            fontWeight: FontWeight.w700,
+                            fontSize: 13,
+                          ),
+                        ),
+                        Icon(Icons.timer),
+                        Text(
+                          '07:00 - 08:30',
+                          style: TextStyle(
+                            fontFamily: 'RobotoMono',
+                            fontWeight: FontWeight.w600,
+                            fontSize: 7,
+                          ),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Android Dev",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black,
-                              fontFamily: 'RobotoMono',
-                            ),
+                    Column(
+                      children: [
+                        Text(
+                          "Membaca",
+                          style: TextStyle(
+                            fontFamily: 'RobotoMono',
+                            fontWeight: FontWeight.w700,
+                            fontSize: 13,
                           ),
-                          Text(
-                            "100 APK Project",
-                            style: TextStyle(
-                              fontSize: 8,
-                              fontFamily: 'RobotoMono',
-                              fontWeight: FontWeight.w600,
-                              letterSpacing: 1,
-                            ),
+                        ),
+                        Icon(Icons.book),
+                        Text(
+                          "13:00 - 13:30",
+                          style: TextStyle(
+                            fontFamily: "RobotoMono",
+                            fontWeight: FontWeight.w600,
+                            fontSize: 7,
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Ngegame",
+                          style: TextStyle(
+                            fontFamily: 'RobotoMono',
+                            fontWeight: FontWeight.w700,
+                            fontSize: 13,
+                          ),
+                        ),
+                        Icon(Icons.laptop),
+                        Text(
+                          "19:15 - 21:00",
+                          style: TextStyle(
+                            fontSize: 7,
+                            fontFamily: 'RobotoMono',
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 5,
-                ),
-                child: Text(
-                  "~ Schedule ~",
-                  style: TextStyle(
-                    fontFamily: 'RobotoMono',
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Text(
-                        'Ngoding',
-                        style: TextStyle(
-                          fontFamily: 'RobotoMono',
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      Icon(Icons.timer),
-                      Text(
-                        '07:00 - 08:30',
-                        style: TextStyle(
-                          fontFamily: 'RobotoMono',
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        "Membaca",
-                        style: TextStyle(
-                          fontFamily: 'RobotoMono',
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      Icon(Icons.book),
-                      Text(
-                        "13:00 - 13:30",
-                        style: TextStyle(
-                          fontFamily: "RobotoMono",
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
